bf31563c05ef88bd864dfee1fc7e237e56e4d818
orcaman <orhiltch@gmail.com>
2014-04-21 16:04:52 +0200

    add tips

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import webapp2
+import jinja2
+import os
 
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True)
+
+v = os.environ.get('latest_static_version')
 
 class Home(webapp2.RequestHandler):
     def get(self):
-        self.redirect('/static/index.html?v=1.1')
+	    template = jinja_environment.get_template('templates/index.html')
+	    self.response.headers['Content-Type'] = 'text/html'
+	    self.response.out.write(template.render(v=v))
 
 app = webapp2.WSGIApplication([
     ('/', Home)
