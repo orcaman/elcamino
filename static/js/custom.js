6fb66ef5a68cbfd9a8c62b7249d7dc361e67b8cb
orcaman <orhiltch@gmail.com>
2014-04-22 09:24:03 +0200

    add share

--- static/js/custom.js
+++ static/js/custom.js
@@ -536,3 +536,9 @@ $("a[data-toggle=\"tab\"]").click(function(e) {
 $('#name').focus(function() {
  $('#success').html('');
 });
+
+function popupwindow(url, title, w, h) {
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);
+  return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+} 
